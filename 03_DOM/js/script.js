75f38ea045fbb541bff4c7f72165d945d468ba4b
PedroZanette <phzanette@gmail.com>
2024-08-29 22:17:55 -0300

    DOM initials

---
+++ 03_DOM/js/script.js
@@ -0,0 +1,11 @@
+// 1 - movendo-se pelo DOM
+console.log(document.body);
+
+console.log(document.body.childNodes);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[1].textContent);
+
